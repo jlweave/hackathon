9fe215aa419432f7f86824b20367ab7c1641244a
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-04-17 14:24:24 -0400

    test/feat: Giphy Service test complete

--- spec/services/giphy_service_spec.rb
+++ spec/services/giphy_service_spec.rb
@@ -14,9 +14,22 @@ RSpec.describe 'Giphy Service' do
       .to_return(status: 200, body: json_response) 
       
       response =  response =  JSON.parse(json_response, symbolize_names: true)
+      # require 'pry';binding.pry
+      expect(response).to be_a(Hash)
+      expect(response[:data]).to be_a(Array)
+      expect(response[:data].count).to be <= (5)
+      
+      response[:data].each do |r|
+        # require 'pry';binding.pry
+        expect(r).to have_key(:type)
+        expect(r[:type]).to eq("gif")
+      
+        expect(r).to have_key(:url)
+        expect(r[:url]).to be_a(String)
 
-      require 'pry';binding.pry
+        expect(r).to have_key(:rating)
+        expect(r[:rating]).to be_a(String)
+      end
     end
-
   end
 end
